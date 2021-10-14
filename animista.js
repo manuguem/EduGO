e56b6889c76cf2238e7c4ce6aa86671e4153c59e
Manuel Guerra <manu.guerra2010@gmail.com>
2021-10-14 00:42:42 -0300

    Updates and several fixes

---
+++ animista.js
@@ -0,0 +1,18 @@
+//$('.sizeImgindex').show().animate({ marginLeft: "0" }, 100).animate({ marginLeft: "3px" }, 300);
+
+
+const typed  = new Typed ('.typed', {
+    //strings: ['Toda la experiencia, herramientas e información para una mejor<br> elección de carrera universitaria en una sola aplicación.'],
+    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 30, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 40, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 2500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+})
